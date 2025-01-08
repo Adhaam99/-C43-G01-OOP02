7739fb551740cf06576b38939f981ed52937888e
adham yehia <adhamyehia194@gmail.com>
2025-01-08 22:44:28 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Assignment
+﻿using System.ComponentModel;
+using System.Threading;
+using Assignment.Structs;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1 - Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            Person person = new Person(3);
+
+            person.AddNewPerson(0, "Adham", 123);
+            person.AddNewPerson(1, "Yehia", 456);
+            person.AddNewPerson(2, "Fathy", 789);
+
+            for (int i = 0; i < person.Size; i++)
+            {
+                Console.WriteLine(person[i]);
+            }
+
+
+
+            #endregion
         }
     }
 }
---
+++ Assignment/Structs/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment.Structs
+{
+    internal struct Person
+    {
+        #region properties
+
+        public string[]? Name { get; set; }
+
+        public int[]? Age { get; set; }
+
+        public int Size { get; set; }
+
+
+        #endregion
+
+        #region Constructors
+
+        public Person(int size)
+        {
+
+            Size = size;
+            Name = new string[Size];
+            Age = new int[Size];
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddNewPerson(int position, string name,  int age)
+        {
+            if (Name is not null && Age is not null)
+            {
+                if (position < Size && position >= 0)
+                {
+                    Name[position] = name;
+                    Age[position] = age;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Getter
+
+        public string this[int index]
+        {
+            get
+            {
+
+                if (Name is not null && Age is not null)
+                {
+                    return $"Index : {index + 1 } :: Name : {Name[index]} :: Age : {Age[index]}";
+                }
+                return "";
+            }
+
+        }
+
+        #endregion
+
+    }
+}
